67265ef3280f055a357add13884c0092e7015d40
Adam DeHaven <adam.dehaven@konghq.com>
2026-01-14 10:53:10 -0500

    chore: remove logging

--- test/fixtures/basic/shared/types/shared-props.ts
+++
@@ -1,3 +0,0 @@
-export interface SharedProps {
-  size: 'small' | 'medium' | 'large'
-}
--- test/fixtures/basic/shared/types/test-button.ts
+++
@@ -1,5 +0,0 @@
-import type { SharedProps } from '#imports'
-
-export interface TestButtonProps extends Pick<SharedProps, 'size'> {
-  appearance: string
-}

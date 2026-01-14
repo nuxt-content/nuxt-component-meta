b8912f2899611227c2d082acc253a0c5c49871c8
Adam DeHaven <adam.dehaven@konghq.com>
2026-01-14 09:25:44 -0500

    test: add playground examples and alias import tests

---
+++ playground/app/components/global/TestButtonInline.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :class="`btn-${size} btn-${appearance}`">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import type { TestButtonInlineProps } from '../../../shared/types/test-button-inline'
+
+const {
+  size = 'medium',
+  appearance = 'default',
+} = defineProps<TestButtonInlineProps>()
+</script>
